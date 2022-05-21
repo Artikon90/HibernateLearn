a864c211dfd27bb13624f3900901f425217a9769
artikon90 <artikon90@gmail.com>
2022-05-22 00:20:43 +0300

    Первый запрос

--- src/main/java/org/artikon90/App.java
+++ src/main/java/org/artikon90/App.java
@@ -1,11 +1,24 @@
 package org.artikon90;
 
+import org.artikon90.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        Person person = session.get(Person.class, 1);
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+        session.getTransaction().commit();
+        sessionFactory.close();
     }
 }
