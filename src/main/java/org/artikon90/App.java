08de0acfe3369d1274b900a601998c72fcb6d39d
artikon90 <artikon90@gmail.com>
2022-05-22 00:21:54 +0300

    Немного улучшил запрос

--- src/main/java/org/artikon90/App.java
+++ src/main/java/org/artikon90/App.java
@@ -14,11 +14,14 @@ public class App {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        Person person = session.get(Person.class, 1);
-        System.out.println(person.getAge());
-        System.out.println(person.getName());
-        session.getTransaction().commit();
-        sessionFactory.close();
+        try {
+            session.beginTransaction();
+            Person person = session.get(Person.class, 1);
+            System.out.println(person.getAge());
+            System.out.println(person.getName());
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
